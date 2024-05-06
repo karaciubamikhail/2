5a1e72276a9e0f6b86d7a723f9cc0e34e4926fd5
Mikhail <134963272+karaciubamikhail@users.noreply.github.com>
2024-05-06 20:15:03 +0300

    2

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path")
 
-module.exports = {
-    entry: ".src/index.js",
-    outpyt: {
+module.export = {
+    entry: ".src//index.js",
+    output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
-        clean: true,
+        clean: true
     },
 };
